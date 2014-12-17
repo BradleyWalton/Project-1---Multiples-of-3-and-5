8a0d33d0090827a07912c763a9eb9858d19f7df7
Bradley <bradley@agenda-group.co.uk>
2014-12-17 21:08:19 +0000

    Completed Solution
    Source code for the completed solution to project 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+
+        System.out.println(sum);
+    }
+
+
+}
